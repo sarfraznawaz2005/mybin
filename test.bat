7bbf0c68c599b8193556e804ad4a2f2694858e82
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2026-01-27 01:39:52 +0500

    ∩╗┐chore: update project files and scripts in mybin directory

--- test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-for /f %%a in ('echo prompt $E^| cmd') do set "CYAN=%%a"
-
-echo %CYAN%[36m----------------------------------------%CYAN%[0m
-echo %CYAN%[36mHello%CYAN%[0m
-echo %CYAN%[36m----------------------------------------%CYAN%[0m
