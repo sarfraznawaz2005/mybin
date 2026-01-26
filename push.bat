37cb6438053b20d804a49b592d808f2a16c7b0b5
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2026-01-27 02:43:19 +0500

    chore: update project files and scripts in mybin directory

--- push.bat
+++ push.bat
@@ -26,7 +26,7 @@ if "%CHANGES_FOUND%" == "true" (
 )
 
 echo %CLR%[38;2;0;255;255m----------------------------------------%CLR%[0m
-echo %CLR%[38;2;0;255;255mNothing to commit, skipping commit step...%CLR%[0m
+echo %CLR%[38;2;255;165;0mNothing to commit, skipping commit step...%CLR%[0m
 echo %CLR%[38;2;0;255;255m----------------------------------------%CLR%[0m
 
 :: Even if there's nothing to commit, we might still need to push if we have commits that haven't been pushed
